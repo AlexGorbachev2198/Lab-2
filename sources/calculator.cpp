34db93edb6690a362b578402ba273918d8692093
AlexGorbachev2198 <Gorbachev2198@gmail.com>
2016-10-21 01:46:50 +0400

    Create calculator.cpp

---
+++ sources/calculator.cpp
@@ -0,0 +1,35 @@
+long double Sum(double x,int y)
+{
+    return x+y;
+}
+long double Dev(double x,int y)
+{
+    if(y!= 0)return x/y;
+    else return 0;
+}
+long double Mult(double x,int y)
+{
+    return x*y;
+}
+long double Sub(double x,int y)
+{
+    return x-y;
+}
+long double Pow(double x,int y)
+{
+    if(y<0) return 1/ Pow(x,-y);
+    else if(y ==0) return 0;
+    else if(y ==1)return x;
+    else return x*Pow(x,y-1);
+}
+long double Sqrt3(double x)
+{
+    double start = 1;
+    int i =1;
+    for(i;i< x;i++)
+    {
+        if (Pow(start,2) == x) break;
+        else start = 0.5*(start + x/start);
+    }
+    return start;
+}
