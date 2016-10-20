dc057ff3f5ffe9fc0d972cfed47b8099d800541f
AlexGorbachev2198 <Gorbachev2198@gmail.com>
2016-10-21 01:40:28 +0400

    Update and rename Calculator.cpp to main.cpp

---
+++ example/main.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include "calculator.hpp"
+
+using namespace std;
+
+int main() {
+    double a;
+    unsigned int b;
+    cin >> a;
+    cin >> b;
+    cout << "Cумма " << Sum(a, b)<< endl;
+    cout << "Частное " << Dev(a, b)<< endl;
+    cout << "Умножение " << Mult(a, b)<< endl;
+    cout << "Разность " << Sub(a, b)<< endl;
+    cout << "Степень " << Pow(a, b)<< endl;
+    cout << "Корень " << Sqrt3(a)<< endl;
+    cin >> a;
+    return 0;
+}
