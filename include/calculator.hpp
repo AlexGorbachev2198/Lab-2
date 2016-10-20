848a1c6456275b89e4fc95e6c95179f37af1e9e1
AlexGorbachev2198 <Gorbachev2198@gmail.com>
2016-10-21 01:44:46 +0400

    Create calculator.hpp

---
+++ include/calculator.hpp
@@ -0,0 +1,6 @@
+long double Sum(double x,int y);
+long double Dev(double x,int y);
+long double Mult(double x,int y);
+long double Sub(double x,int y);
+long double Pow(double x,int y);
+long double Sqrt3(double x);
