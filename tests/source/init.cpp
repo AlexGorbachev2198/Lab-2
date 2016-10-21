99e3b8898c18207fcdb1465e4206fde651ef13cf
AlexGorbachev2198 <Gorbachev2198@gmail.com>
2016-10-21 14:19:32 +0400

    Update init.cpp

--- tests/source/init.cpp
+++ tests/source/init.cpp
@@ -2,49 +2,49 @@
 #include <catch.hpp>
 
 SCENARIO("calculator Sum", "[Sum]") {
-    int x = 3;
+    double x = 3;
     int y = 4;
     
-    int rv = sum( x, y );
+    double rv = sum( x, y );
     
 	REQUIRE( rv == 7);
 }
 SCENARIO("calculator Dev", "[Dev]") {
-    int x = 8;
+    double x = 8;
     int y = 4;
     
-    int rv = Dev( x, y );
+    double rv = Dev( x, y );
     
 	REQUIRE( rv == 2);
 }
 SCENARIO("calculator Mult", "[Mult]") {
-    int x = 3;
+    double x = 3;
     int y = 4;
     
-    int rv = Mult( x, y );
+    double rv = Mult( x, y );
     
 	REQUIRE( rv == 12);
 }
 SCENARIO("calculator Sub", "[Sub]") {
-    int x = 5;
+    double x = 5;
     int y = 4;
     
-    int rv = Sub( x, y );
+    double rv = Sub( x, y );
     
 	REQUIRE( rv == 1);
 }
 SCENARIO("calculator Pow", "[Pow]") {
-    int x = 3;
+    double x = 3;
     int y = 2;
     
-    int rv = Pow( x, y );
+    double rv = Pow( x, y );
     
 	REQUIRE( rv == 9 );
 }
 SCENARIO("calculator Sqrt3", "[Sqrt3]") {
-    int x = 4;
+    double x = 4;
     
-    int rv = Sqrt3( x );
+    double rv = Sqrt3( x );
     
 	REQUIRE( rv == 2);
 }
