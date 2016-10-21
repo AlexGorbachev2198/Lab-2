516dbf9e3b7f3094047985f202d7c9910308d282
AlexGorbachev2198 <Gorbachev2198@gmail.com>
2016-10-21 14:45:06 +0400

    Update calculator.hpp

--- include/calculator.hpp
+++ include/calculator.hpp
@@ -2,5 +2,5 @@ long double Sum(double x,int y);
 long double Dev(double x,int y);
 long double Mult(double x,int y);
 long double Sub(double x,int y);
-long double Pow(double x,int y);
+long double Pow1(double x,int y);
 long double Sqrt3(double x);
