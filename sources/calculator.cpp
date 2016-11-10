714c194239ac264e27e9a75a1424bdc0438864f8
AlexGorbachev2198 <Gorbachev2198@gmail.com>
2016-11-11 03:08:56 +0400

    Update calculator.cpp

--- sources/calculator.cpp
+++ sources/calculator.cpp
@@ -31,15 +31,17 @@ long double Pow1(double x,int y)
 }
 long double Sqrt3(double x)
 {
-    double pu =1; double y;
-    int i=0;
-    while(1) {
-        y=pu;
-        pu=0.5*(pu+x/pu);
-        if (pu>=y) {
-            i++; if (i>1);
-            break;
+    float xn = 1.0;
+    float xn1 = 2.0;
+    if ( x != 1 && x != 0 )
+    {
+        while ( (xn1 - xn) > 0.000001 || (xn1 - xn) < -0.000001  )
+        {
+            xn = xn1;
+            xn1 = (float)(xn + (float)x/xn)/2;
         }
     }
-    return(pu);
+    else if ( x == 1 ) xn1 = 1;
+    else if ( x == 0 ) xn1 = 0;
+    return xn1;
 }
