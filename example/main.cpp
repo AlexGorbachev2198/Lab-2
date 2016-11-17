26daff51ceb3385dadd1d9e8f0961e9a033a7121
AlexGorbachev2198 <Gorbachev2198@gmail.com>
2016-11-17 19:09:47 +0300

    Update main.cpp

--- example/main.cpp
+++ example/main.cpp
@@ -8,14 +8,16 @@ int main() {
     unsigned int b;
     cin >> a;
     cin >> b;
+    if(cin){
     cout << "Cумма " << Sum(a, b)<< endl;
     long double *buf = Dev(a,b);
     if (buf) cout << "Частное: " << *buf << endl;
-    else cout << "Ошибка. Ноль в знаменателе" << endl;
+    else cout << "На ноль делить нельзя." << endl;
     cout << "Умножение " << Mult(a, b)<< endl;
     cout << "Разность " << Sub(a, b)<< endl;
     cout << "Степень " << Pow1(a, b)<< endl;
     cout << "Корень " << Sqrt3(a)<< endl;
-    cin >> a;
-    return 0;
+    }
+    else cout << "Неправильный ввод";
+    system("pause");
 }
